f7c90ba36933e30c3d9d5f873c22c7839632f283
Ellis Kenyo <elken.tdos@gmail.com>
2015-10-27 22:13:31 +0000

    PEP8 stuff

--- build.py
+++ build.py
@@ -18,7 +18,8 @@ def get_file_with_progress(url, file, do_tar=True, path="."):
     with open(file, "wb") as f:
         print("Downloading %s" % file)
         total_length = int(r.headers.get('content-length'))
-        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+        for chunk in progress.bar(r.iter_content(chunk_size=1024),
+                                  expected_size=(total_length/1024) + 1):
             if chunk:
                 f.write(chunk)
                 f.flush()
@@ -42,7 +43,9 @@ def get_file_with_progress(url, file, do_tar=True, path="."):
 def check_paths():
     for dep_name, val in paths.items():
         if not os.path.exists(val):
-            if input("%s not found in expected location, enter other path? [N/y] " % dep_name).lower() in ['yes', 'y']:
+            if input("%s not found in expected location, "
+                     "enter other path? [N/y] " % dep_name).lower() in ['yes',
+                                                                        'y']:
                 while True:
                     path = input("Enter path for %s: " % dep_name)
                     if os.path.exists(path):
@@ -51,7 +54,8 @@ def check_paths():
                     else:
                         print("Path invalid, try again")
             else:
-                if input("Install %s? [Y/n] " % dep_name).lower() in ['yes', 'y', '']:
+                if input("Install %s? [Y/n] " % dep_name).lower() in ['yes',
+                                                                      'y', '']:
                     eval("install_" + dep_name.lower() + "()")
                 else:
                     print("%s not found, continuing anyway" % dep_name)
@@ -65,11 +69,14 @@ def install_cmake():
         old_dir = os.getcwd()
         os.chdir(tmp_dir)
 
-        get_file_with_progress("http://cmake.org/files/v3.3/cmake-3.3.2-win32-x86.exe",
-                               "cmake-3.3.2-win32-x86.exe",
+        file = "cmake-3.3.2-win32-x86.exe"
+        get_file_with_progress("http://cmake.org/files/v3.3/" + file,
+                               file,
                                False)
 
-        subprocess.call(["cmake-3.3.2-win32-x86.exe", "/S", "/D=C:\Program Files (x86)\CMake"],
+        subprocess.call(["cmake-3.3.2-win32-x86.exe",
+                         "/S",
+                         "/D=C:\Program Files (x86)\CMake"],
                         stdout=subprocess.PIPE,
                         shell=True)
         os.chdir(old_dir)
@@ -81,7 +88,8 @@ def install_boost():
         old_dir = os.getcwd()
         os.chdir(tmp_dir)
 
-        get_file_with_progress("http://sourceforge.net/projects/boost/files/latest/download?source=files",
+        get_file_with_progress("http://sourceforge.net/projects/boost/files/la"
+                               "test/download?source=files",
                                "boost.tar.bz2",
                                True)
 
@@ -97,12 +105,14 @@ def install_qt():
         old_dir = os.getcwd()
         os.chdir(tmp_dir)
 
-        get_file_with_progress("http://download.qt.io/official_releases/qt/5.5/5.5.1/"
+        get_file_with_progress("http://download.qt.io/official_releases/qt/5.5"
+                               "/5.5.1/",
                                "qt-opensource-windows-x86-mingw492-5.5.1.exe",
                                "qt-opensource-windows-x86-mingw492-5.5.1.exe",
                                False)
 
-        subprocess.call("qt-opensource-windows-x86-mingw492-5.5.1.exe", stdout=subprocess.PIPE)
+        subprocess.call("qt-opensource-windows-x86-mingw492-5.5.1.exe",
+                        stdout=subprocess.PIPE)
         os.chdir(old_dir)
 
 
@@ -114,17 +124,24 @@ def run_cmake():
     mingw_bin_dir = ""
     lib_cmake_dir = ""
     pa_cmake_dir = ""
-    if not os.path.exists("CMakeLists.txt") or not os.path.exists("../CMakeLists.txt"):
-        if input("Would you like to download the Project Ascension source? [Y/n] ").lower() in ["yes", "y", ""]:
+    if not os.path.exists("CMakeLists.txt") or \
+       not os.path.exists("../CMakeLists.txt"):
+        if input("Would you like to download the "
+                 "Project Ascension source? [Y/n] ").lower() in ["yes",
+                                                                 "y",
+                                                                 ""]:
             pa_cmake_dir = clone_repo()
         else:
             while True:
-                pa_cmake_dir = input("Enter the path of the Project Ascension source code (with CMakeLists.txt in) ")
-                if os.path.exists(os.path.join(pa_cmake_dir, "CMakeLists.txt")):
+                pa_cmake_dir = input("Enter the path of the Project Ascension "
+                                     "source code (with CMakeLists.txt in) ")
+                if os.path.exists(os.path.join(pa_cmake_dir,
+                                               "CMakeLists.txt")):
                     print("Found source code, building")
                     break
                 else:
-                    print("Directory is missing CMakeLists.txt, please try again.")
+                    print("Directory is missing CMakeLists.txt, "
+                          "please try again.")
     for folder, subs, files in os.walk(paths["Qt"]):
         if folder.endswith("mingw492_32\lib\cmake"):
             lib_cmake_dir = folder
@@ -136,9 +153,10 @@ def run_cmake():
     if lib_cmake_dir == "" or mingw_bin_dir == "" or qt_dll_dir == "":
         print("Qt installation is broken, try re-installing.")
     else:
-        print("Running cmake -G \"MinGW MakeFiles\" -DWITH_TESTS=true -DCMAKE_PREFIX_PATH=\"%s\" %s"
+        print("Running cmake -G \"MinGW MakeFiles\" -DWITH_TESTS=true "
+              "-DCMAKE_PREFIX_PATH=\"%s\" %s"
               % (lib_cmake_dir, pa_cmake_dir))
-        os.environ["PATH"] = "C:\Program Files (x86)\CMake\\bin" + os.pathsep + mingw_bin_dir
+        os.environ["PATH"] = paths["CMake"] + os.pathsep + mingw_bin_dir
         print(os.environ["PATH"])
         subprocess.call(["cmake",
                          "-G",
@@ -163,7 +181,7 @@ def clone_repo():
     clone_dir = ""
     while True:
         clone_dir = input("Where would you like to save Project Ascension? "
-                          "\n(If the path doesn't exist, it will be created.) ")
+                          "\n(If the path doesn't exist,it will be created.) ")
 
         if clone_dir is "":
             print("Invalid path entered. Try again.")
@@ -178,7 +196,8 @@ def clone_repo():
     with tempfile.TemporaryDirectory() as tmp_dir:
         old_dir = os.getcwd()
         os.chdir(tmp_dir)
-        get_file_with_progress("http://github.com/Proj-Ascension/Client/archive/dev.zip", "dev.zip", True, clone_dir)
+        get_file_with_progress("http://github.com/Proj-Ascension/Client/"
+                               "archive/dev.zip", "dev.zip", True, clone_dir)
 
         os.chdir(old_dir)
 
